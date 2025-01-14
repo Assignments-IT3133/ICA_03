8bd004a4e22c9ca7407f556ed747a8947eb53ac5
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-01-14 21:58:42 +0530

    feat: :sparkles: Enable navigation to Profile component in App

--- list/App.js
+++ list/App.js
@@ -3,6 +3,7 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import StudentList from './components/StudentList';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
+import Profile from './components/Profile';
 
 export default function App() {
   const Stack=createNativeStackNavigator();
@@ -11,7 +12,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name='StudentList' component={StudentList}/>
-          {/* <Stack.Screen name='Profile' component={Profile}/> */}
+          <Stack.Screen name='Profile' component={Profile}/>
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
