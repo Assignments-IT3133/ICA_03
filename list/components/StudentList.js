2c5a7a96c06bac9c586f706a006a5017bdffe5b4
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-01-14 21:58:31 +0530

    feat: :sparkles: Enable navigation to Profile component from StudentList on item press

--- list/components/StudentList.js
+++ list/components/StudentList.js
@@ -1,14 +1,20 @@
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { students } from './StudentDb';
+import {useNavigation} from '@react-navigation/native'
 
 export default function StudentList() {
+    const navigation=useNavigation();
     return (
         <View style={styles.container}>
             <FlatList 
                 data={students}
                 keyExtractor={item => item.id.toString()}
-                renderItem={({ item }) => <Text style={styles.text}>{item.name}</Text>}
+                renderItem={({ item }) => 
+                <TouchableOpacity onPress={()=>navigation.navigate('Profile',{student:item})}>
+                <Text style={styles.text}>{item.name}</Text>
+                </TouchableOpacity>
+            }
             />
         </View>
     );
