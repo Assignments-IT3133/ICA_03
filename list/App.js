287314fdc5fb68dde463ac1468375f834e9dd244
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-01-14 21:13:20 +0530

    feat: :sparkles: Implement navigation and integrate StudentList component in App

--- list/App.js
+++ list/App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import StudentList from './components/StudentList';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
+  const Stack=createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='StudentList' component={StudentList}/>
+          {/* <Stack.Screen name='Profile' component={Profile}/> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
